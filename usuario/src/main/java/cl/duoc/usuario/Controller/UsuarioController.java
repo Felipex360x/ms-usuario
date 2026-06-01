49a4d19867133720bfaf3bb7fb9c160c593787fe
felipe acuna <acunafelipe272@gmail.com>
2026-06-01 17:05:29 +0000

    feat(auth): documentacion

--- usuario/src/main/java/cl/duoc/usuario/Controller/UsuarioController.java
+++ usuario/src/main/java/cl/duoc/usuario/Controller/UsuarioController.java
@@ -18,11 +18,17 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//* nuevos imports */
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 
+@Tag(name="Usuarios",description = "Operaciones de gestion de usuarios")
 @RestController
 @RequestMapping("/api/v2/usuarios")
 public class UsuarioController {
@@ -30,31 +36,65 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+
+    @Operation(
+        summary = "Listar todas los Usuarios",
+        description = "Retorna la lista completa de Usuarios registradas en el sistema."
+    )
+    @ApiResponse(responseCode = "200", description = "Lista obtenida exitosamente")
+
+
     @GetMapping
     public ResponseEntity<List<UsuarioDTO>>getAll(){
         return ResponseEntity.ok(usuarioService.findAll());
     }
 
+    @Operation(summary = "Buscar Usuario por ID")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Usuario encontrada"),
+        @ApiResponse(responseCode = "404", description = "Usuario no encontrada")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<UsuarioDTO> getById(@PathVariable Long id) {
+    public ResponseEntity<UsuarioDTO> getById(@Parameter(description = "Id unico de la mascota",required = true)@PathVariable Long id) {
         return ResponseEntity.ok(usuarioService.findById(id));
     }
 
+
+
+
+
+    @Operation(summary = "Registrar nueva Usuario")
+    @ApiResponse(responseCode = "201", description = "Usuario creada exitosamente")
     @PostMapping
     public ResponseEntity<UsuarioDTO> crear(@Valid @RequestBody UsuarioCreateDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.crear(dto));
     }
 
+    @Operation(summary = "Actualizar Usuario existente")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Actualización exitosa"),
+        @ApiResponse(responseCode = "404", description = "Usuario no encontrada")
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<UsuarioDTO> actualizar(@PathVariable Long id, @Valid @RequestBody UsuarioCreateDTO dto) {
+    public ResponseEntity<UsuarioDTO> actualizar(@Parameter(description = "Ide de la mascota a actualizar")@PathVariable Long id, @Valid @RequestBody UsuarioCreateDTO dto) {
         return ResponseEntity.ok(usuarioService.actualizar(id, dto));
     }
 
+    @Operation(summary = "Eliminar Usuario")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Eliminación exitosa"),
+        @ApiResponse(responseCode = "404", description = "Usuario no encontrada")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+    public ResponseEntity<Void> eliminar(@Parameter(description = "Ide de la mascota a eliminar")@PathVariable Long id) {
         usuarioService.eliminar(id);
         return ResponseEntity.noContent().build();
     }
+
+
+
+
+
 
     @PostMapping("/manual")
     public ResponseEntity<?> crearConValidacionManual(@RequestBody UsuarioCreateDTO dto){
